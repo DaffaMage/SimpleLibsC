3cef88f4a0207211a1f220ba48bcccd70db369ff
DESKTOP-9RG54CS\Daffa Mage <daffaabiyyum@gmail.com>
2018-07-13 09:01:39 +0700

    Header documentation added

--- primenum.h
+++ primenum.h
@@ -1,6 +1,17 @@
 #ifndef PRIMENUM_H_INCLUDED
 #define PRIMENUM_H_INCLUDED
 
+/*
+** primenum.h is a file made by Daffa the Mage (aka DaffaMage)
+** This file is meant to extend C functionality by adding functions related to Prime Number
+*/
+
+/*
+** API
+** isPrime take int and return a boolean in "int" form
+** isNotPrime is the same as isPrime, but reversed check
+** printPrimeListA/B/C takes some int arguments that will be used as parameter for the printed prime numbers
+*/
 
 int isPrime(int x); //check a number if it's prime or not (return 1 when true and 0 when false)
 int isNotPrime(int x); //a reversal check of isPrime
